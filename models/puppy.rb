2fedee4cf94de3271e3f3264b08e2401b6306293
Srikalyan Megati <kalmegati@gmail.com>
2019-12-12 16:11:05 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -2,10 +2,10 @@ class Puppy
   
   attr_accessor :name, :breed, :age
   
-  def initialize(name, breed, age)
-    @name = name
-    @breed = breed
-    @age = age
+  def initialize(attrs)
+    @name = attrs[:name]
+    @breed = attrs[:breed]
+    @age = attrs[:age]
   end
   
 end
